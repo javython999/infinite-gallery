ffd9a264be37520e9d2432284b4aa2a5f9189bcb
jjy <infinity2544@gamil.com>
2020-10-22 21:24:00 +0900

    안내메세지 수정

--- src/components/imageRender/imageRender.jsx
+++ src/components/imageRender/imageRender.jsx
@@ -17,7 +17,7 @@ const ImageRender = (props) => {
   return(
     <div className={styles.div} >
       <img className={styles.img} onClick={refresh} src={url} alt='img'/>
-      <h4>이미지 클릭시 새로운 이미지가 나옵니다.</h4>
+      <h4>이미지 클릭시 새로운 로딩됩니다.</h4>
       <h4>새로운 이미지 로딩은 10초간격으로 가능합니다.</h4>
     </div>
   )
